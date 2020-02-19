c70e78f19cee0d040e5a4a908169106a64c2edad
Terry Threatt <terry.threatt@gmail.com>
2020-02-19 04:52:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(str)
   array = str.split
+  new_array = []
   array.each do |l|
-    puts l.reverse.split.reverse.join(" ")
+    new_array.push(l.reverse)
   end
+    puts "#{new_array.join(" ")}"
 end 
